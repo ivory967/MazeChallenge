f71ffd398880cc1905ced2a24164dec89bb47aa0
Carlos Rios <ivory967@hotmail.com>
2024-06-21 15:16:17 -1000

    Add project files.

---
+++ MazeChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void method(string[] maze)
+{
+	foreach (var item in maze)
+	{
+		Console.WriteLine(item);
+	}
+}
+method(["Hola", "Cagon"]);
