6b8cb5def35f1ed9a5e71f72ca3cb4d5b8bc7ac8
Rios Espinal, Carlos <carlos.rios@corteva.com>
2024-06-25 09:29:32 -1000

    trying the encapsule class

--- MazeChallenge.Shared/Mirror.cs
+++ MazeChallenge.Shared/Mirror.cs
@@ -13,5 +13,27 @@ namespace MazeChallenge.Shared
         public char Direction { get; set; } // Right(R) of Left (L)
         public Enumerables.MirrorType MirrorType { get; set; }
         public char ReflectiveSide { get; set; } //Top (T) or Bottom (B)
+
+        public Mirror(int x, int y, char direction, Enumerables.MirrorType mirrorType, char reflectiveSide)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
+            MirrorType = mirrorType;
+            ReflectiveSide = reflectiveSide;
+        }
+        public char Reflect(char orientation)
+        {
+            if (ReflectiveSide =='B' || ReflectiveSide =='T')
+            {
+                return orientation switch
+                {
+                    'H' => 'V',
+                    'V' => 'H',
+                    _ => throw new InvalidOperationException()
+                };
+            }
+            return orientation;
+        }
     }
 }
